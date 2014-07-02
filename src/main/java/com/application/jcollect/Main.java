fbeb6bf0737f328085c1beca523884649b20a97c
Enrico Bianchi <enrico.bianchi@gmail.com>
2014-07-02 23:08:20 +0200

    Project initialization

---
+++ src/main/java/com/application/jcollect/Main.java
@@ -0,0 +1,17 @@
+/*
+ Copyright (C) 2014 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       JCollect
+ Description   A collectd system clone
+ License       GPL version 2 (see GPL.txt for details)
+ */
+package com.application.jcollect;
+
+/**
+ *
+ * @author enrico
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO: add code
+    }
+}
