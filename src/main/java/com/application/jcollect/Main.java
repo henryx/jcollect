9c585b1d30d7748ea647546665f0560a9315dd0a
Enrico Bianchi <enrico.bianchi@gmail.com>
2014-07-03 00:38:59 +0200

    Added method for starting application

--- src/main/java/com/application/jcollect/Main.java
+++ src/main/java/com/application/jcollect/Main.java
@@ -6,9 +6,18 @@
  */
 package com.application.jcollect;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 /**
  *
@@ -32,11 +41,43 @@ public class Main {
 
     public void printHelp(Integer code) {
         HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("Memento", this.opts);
+        formatter.printHelp("JCollect", this.opts);
         System.exit(code);
     }
 
+    public void go(String[] args) throws ParseException, FileNotFoundException, IOException, InterruptedException {
+        CommandLine cmd;
+        CommandLineParser parser;
+        Properties cfg;
+        int interval;
+
+        parser = new PosixParser();
+        cmd = parser.parse(this.opts, args);
+
+        cfg = new Properties();
+        cfg.load(new FileInputStream(new File(cmd.getOptionValue("cfg"))));
+
+        interval = Integer.parseInt(cfg.getProperty("interval"));
+        while (true) {
+            // TODO add code for execute collectors systems
+            Thread.sleep(interval * 1000);
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: add code
+        Main app;
+
+        app = new Main();
+        try {
+            app.go(args);
+        } catch (ParseException ex) {
+            app.printHelp(1);
+        } catch (FileNotFoundException ex) {
+            System.err.println("Configuration file not found");
+        } catch (IOException ex) {
+            System.err.println("Error when opening configuration file");
+        } catch (InterruptedException ex) {
+            System.err.println("Interrupted");
+        }
     }
 }
