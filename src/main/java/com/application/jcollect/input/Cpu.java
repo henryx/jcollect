4224845f2f6b86429217ac2eeb593ca459966370
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-06 17:36:43 +0200

    Added class for getting CPU metrics

---
+++ src/main/java/com/application/jcollect/input/Cpu.java
@@ -0,0 +1,27 @@
+/*
+ Copyright (C) 2016 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       JCollect
+ Description   A collectd system replacement
+ License       GPL version 2 (see GPL.txt for details)
+ */
+package com.application.jcollect.input;
+
+import org.ini4j.Profile.Section;
+
+/**
+ *
+ * @author ebianchi
+ */
+public class Cpu extends Input {
+    
+    public Cpu(Section section) {
+        super(section);
+    }
+
+    @Override
+    public void run() {
+        String hostname = this.getHostname();
+        String os = this.getOs();
+    }
+    
+}
