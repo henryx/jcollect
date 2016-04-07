9f2325957be55cc51998b3e45804707328e4212c
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-08 00:32:00 +0200

    Added most of code for writing data

--- src/main/java/com/application/jcollect/input/Cpu.java
+++ src/main/java/com/application/jcollect/input/Cpu.java
@@ -6,6 +6,7 @@
  */
 package com.application.jcollect.input;
 
+import java.util.LinkedHashMap;
 import org.ini4j.Profile.Section;
 
 /**
@@ -13,15 +14,24 @@ import org.ini4j.Profile.Section;
  * @author ebianchi
  */
 public class Cpu extends GenericInput {
-    
+
     public Cpu(Section section) {
         super(section);
     }
 
     @Override
     public void run() {
-        String hostname = this.getHostname();
-        String os = this.getOs();
+        LinkedHashMap<String, String> data;
+
+        data = new LinkedHashMap<>();
+
+        data.put("hostname", this.getHostname());
+        data.put("os", this.getOs());
+        // TODO: getting Cpu metrics
+
+        this.output.setName("Cpu");
+        this.output.setData(data);
+        this.output.write();
     }
-    
+
 }
