7f995f890a701470f7de6c04ead3b511252e2c34
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-13 12:43:58 +0200

    Added code for per CPU load

--- src/main/java/com/application/jcollect/input/Cpu.java
+++ src/main/java/com/application/jcollect/input/Cpu.java
@@ -16,39 +16,52 @@ import oshi.hardware.CentralProcessor;
  * @author ebianchi
  */
 public class Cpu extends GenericInput {
+
     private final CentralProcessor cpu;
 
     public Cpu(Section section) {
         super(section);
-        
+
         SystemInfo si;
-        
+
         si = new SystemInfo();
         this.cpu = si.getHardware().getProcessor();
     }
 
     private double getCpuLoadAverage() {
         double load;
-        
+
         load = this.cpu.getSystemLoadAverage();
         return load;
     }
-    
+
     private double getCpuLoad() {
         double load;
-        
+
         load = this.cpu.getSystemCpuLoad() * 100;
         return load;
-    } 
+    }
+
+    private double[] getCpuLoads() {
+        double[] loads;
+
+        loads = this.cpu.getProcessorCpuLoadBetweenTicks();
+        return loads;
+    }
 
     @Override
     public void run() {
         LinkedHashMap<String, String> data;
+        double[] loads = new double[4];
 
         data = new LinkedHashMap<>();
+        loads = this.getCpuLoads();
 
         data.put("hostname", this.getHostname());
         data.put("os", this.getOs());
+        for (int i = 0; i < loads.length; i++) {
+            data.put("cpu" + (i + 1), Double.toString(loads[i] * 100));
+        }
         data.put("load", Double.toString(this.getCpuLoad()));
         data.put("loadavg", Double.toString(this.getCpuLoadAverage()));
         // TODO: getting Cpu metrics
