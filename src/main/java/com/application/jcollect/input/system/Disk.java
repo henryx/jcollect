17a336ff430ccf88a6cbe028343a3a825a67128a
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-05-03 23:48:13 +0200

    Implemented disk statistics collector

--- src/main/java/com/application/jcollect/input/system/Disk.java
+++ src/main/java/com/application/jcollect/input/system/Disk.java
@@ -7,22 +7,37 @@
 package com.application.jcollect.input.system;
 
 import com.application.jcollect.input.Input;
+import java.util.LinkedHashMap;
 import org.ini4j.Profile.Section;
+import oshi.SystemInfo;
+import oshi.hardware.HWDiskStore;
 
 /**
  *
  * @author enrico.bianchi@gmail.com
  */
 public class Disk extends Input {
+
     private final String metricName = "Disk";
+    private SystemInfo si;
 
     public Disk(Section section) {
         super(section);
+
+        si = new SystemInfo();
     }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        LinkedHashMap<String, String> data;
 
+        for (HWDiskStore store : this.si.getHardware().getDiskStores()) {
+            data = new LinkedHashMap<>();
+            data.put("name", store.getName());
+            data.put("reads", Long.toString(store.getReads()));
+            data.put("writes", Long.toString(store.getWrites()));
+
+            this.write(this.metricName, data);
+        }
+    }
 }
