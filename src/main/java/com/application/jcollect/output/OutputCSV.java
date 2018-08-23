b1e3c0a8207a631c0190a071e1a0e79f0c2533ca
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-08-24 00:47:01 +0200

    Added header to CSV output if file not exist

--- src/main/java/com/application/jcollect/output/OutputCSV.java
+++ src/main/java/com/application/jcollect/output/OutputCSV.java
@@ -9,6 +9,9 @@ package com.application.jcollect.output;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.ini4j.Profile.Section;
@@ -29,12 +32,26 @@ public class OutputCSV extends Output {
     @Override
     public void write() {
         String fileName;
+        Path path;
+        boolean header;
         long now;
 
         now = System.currentTimeMillis() / 1000L;
         fileName = this.section.get("path") + File.separator + this.name.toLowerCase() + ".csv";
-
+        path = Paths.get(fileName);
+        header = !Files.exists(path);
+        
         try (FileWriter writer = new FileWriter(fileName, true)) {
+            if (header) {
+                writer.append("date");
+                writer.append(this.COMMA_DELIMITER);
+                for (String key : this.data.keySet()) {
+                    writer.append(key);
+                    writer.append(this.COMMA_DELIMITER);
+                }
+                writer.append(this.NEW_LINE_SEPARATOR);
+            }
+
             writer.append(Long.toString(now));
             writer.append(this.COMMA_DELIMITER);
             for (String key : this.data.keySet()) {
